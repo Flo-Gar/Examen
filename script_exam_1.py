4276a079e1fe9b175948e28e336bc86b48a0f187
Flo-Gar <52507351+Flo-Gar@users.noreply.github.com>
2019-07-05 09:17:14 -0500

    Add files via upload

---
+++ script_exam_1.py
@@ -0,0 +1,3 @@
+import math
+x=float(input('Escribe el numero a operar: '))
+print('El resultado es ',r)
---
+++ script_exam_2.py
@@ -0,0 +1,3 @@
+x=float(input('Dame el numero a operar: '))
+r = x**4-10*x**3+3*x**2+x*23
+print('El resultado es ',r)
